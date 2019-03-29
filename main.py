481662aff871460b0ed7f156d7072d68566edf6b
Clara <rustiktie@gmail.com>
2019-03-29 13:37:49 +0100

    loading images from user input

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
 import numpy as np
 import cv2
 
+# User input
+targetPath = input("Enter the target image: ")
+imagePath = input("Enter the image: ")
+threshold = input("Enter the threshold: ")
+
+# Load images
+target = cv2.imread(targetPath, 0)
+image = cv2.imread(imagePath, 0)
+
+
+
+
+
