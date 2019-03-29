27e89fd542732577ecc7193e5f193375b0f8e3c3
Clara <rustiktie@gmail.com>
2019-03-29 14:04:46 +0100

    saving rows and cols

--- main.py
+++ main.py
@@ -9,8 +9,16 @@ threshold = input("Enter the threshold: ")
 # Load images
 target = cv2.imread(targetPath, 0)
 image = cv2.imread(imagePath, 0)
+colorImage = cv2.imread(imagePath, 1)
+colorTarget = cv2.imread(targetPath, 1)
+kRows,kCols = target.shape
+rows, cols = image.shape
+
+# Create matching map
 
 
 
+# Print results
 
-
+cv2.imshow("Image", colorImage)
+cv2.imshow("Target", colorTarget)
